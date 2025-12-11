1c782700fd4f2a7e30dd957ee140e0bd1b8e77ec
ys Asesa Nitaicandra <abinashshaw16@gmail.com>
2025-12-11 17:06:42 +0530

    Working Project Added Method to get Response from OpenAI

--- src/main/java/com/krishna/openai/OpenAI/controller/OpenAIController.java
+++ src/main/java/com/krishna/openai/OpenAI/controller/OpenAIController.java
@@ -1,6 +1,10 @@
 package com.krishna.openai.OpenAI.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenAIController {
 
-    @PostMapping("/chat")
-    private ResponseEntity<String> chat(
+    private ChatClient chatClient;
+
+    public OpenAIController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> chat(
             @RequestParam(value = "q", required = true) String q){
                 return ResponseEntity.ok("Hello From Krishna OpenAI Project");
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> openAIChat(
+            @RequestParam(value = "q", required = true) String q){
+        return ResponseEntity.ok(chatClient.prompt(q).call().content());
     }
 
 }
